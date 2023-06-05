d6369ef5a160fe26ec8128d52aba524584a28b07
Jeremy L <mail.jeremy.li@gmail.com>
2023-06-05 21:22:47 +1000

    moved sandbox

--- sandbox/repl-sandbox.ts
+++ sandbox/repl-sandbox.ts
@@ -1,4 +1,4 @@
-import * as library from 'ts-maths';
+import * as library from '../src/ts-maths';
 
 // assign library to global object so it can be accessed in REPL sandbox
 (global as any).tsMaths = library;
